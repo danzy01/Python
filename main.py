13634f157fefb2ee96b04ef199658596fb7ddda6
Daniel <danzy01@interia.pl>
2022-07-02 11:59:22 +0200

    SQL potynga uniwersalne funkcje

--- main.py
+++ main.py
@@ -11,6 +11,85 @@ def tpl_to_str(a):
         str = str + " " + i
     return str
 # Press the green button in the gutter to run the script.
+def conv_to_parstr(nazwy):
+    str_temp = ""
+
+    i = 0
+
+    ilosc = int(len(nazwy)) - 1
+    while i <= ilosc:
+
+        if i < ilosc:
+            str_temp = str_temp + nazwy[i] + ","
+            i = i + 1
+        if i == 1:
+            str_temp = str_temp + drg[i] + " "
+            i = i + 1
+    return str_temp
+
+def insert_data(data:mysql.connector.connection_cext.CMySQLConnection,nazwa):
+    columns = input('Enter your column names with comma {,} eg.( Name,Surname ):')
+    data = input('Enter data for your columns with space: eg. (data_column1 data_column2) :'.format(columns))
+    sql_in_data = data.split()
+    num = columns.split(',')
+    ilosc = int(len(num))
+    print(ilosc)
+    temp = ["{}"]*ilosc
+    temp_t = tuple(temp)
+    temp_str = ""
+    i=0
+    while i < ilosc:
+        if i < ilosc-1:
+            temp_str = temp_str +"'"+ temp[i]+"'" +","
+        if i == ilosc-1:
+            temp_str = temp_str +"'"+ temp[i]+"'"
+        i=i+1
+    temp_sql_in = "INSERT INTO {name} ({str}) VALUES ({mod})".format(name=nazwa, str=columns, mod=temp_str)
+    print(temp_sql_in)
+    sql_in = temp_sql_in.format(*sql_in_data)
+
+    try:
+        print(sql_in)
+        #data.cursor().execute(beg)
+        #data.commit()
+    except:
+        print("fail")
+
+
+
+
+def config_data(data:mysql.connector.connection_cext.CMySQLConnection):
+    name_table = input('Enter the name of your table:')
+    columns = input('Enter your column names with space eg.( Name Surname ):')
+
+
+    #Data processing
+    columns_work = columns.split()
+    ilosc = int(len(columns_work)) - 1
+    b = ["VARCHAR(255)"] * (ilosc + 1)
+    x = tuple(b)
+    str = ""
+    #Petla przygotowywujaca string
+    i = 0
+
+    while i <= ilosc:
+
+        if i < ilosc:
+            str = str + " " + columns_work[i] + " " + x[i] + ","
+            i = i + 1
+        if i == 1:
+            str = str +" " + columns_work[i] + " " + x[i]
+            i = i + 1
+
+    beg = "CREATE TABLE {name} (id INT AUTO_INCREMENT PRIMARY KEY,".format(name=name_table) + str + ")"
+    #Proba utworzenia tabeli
+    try:
+        data.cursor().execute(beg)
+    except:
+        print('Unable to create table of data')
+
+
+
 if __name__ == '__main__':
 
 
@@ -22,7 +101,8 @@ if __name__ == '__main__':
     except:
         print('Polaczenie nie zostalo nawiazane z baza danych')
 
-    cur = data.cursor()
+    #cur = data.cursor()
+
 
     """   STARTER DO PRZEROBKI
         #print(cur.execute("SELECT name FROM users;"))
@@ -39,42 +119,54 @@ if __name__ == '__main__':
     nazwa = "Awesome"
     opcja = int(input("Podaj opcje: "))
     while opcja != 0:
+        #Configuration
         if opcja == 1:
-            bing = "SELECT {},{} FROM {nazwaa}".format(*drg,nazwaa=nazwa)
-            emk = ["{}"]*3
-            str_tst = tpl_to_str(tuple(emk))
-            ciekawe = "no {}".format(str_tst)
-            print(ciekawe.format(1,2,3))
-            print(bing)
+
+            config_data(data)
+            #print(good_config(nazwa,drg))
+            #print(type(data))
+            #bing = "SELECT {},{} FROM {nazwaa}".format(*drg,nazwaa=nazwa)
+            #emk = ["{}"]*3
+            #str_tst = tpl_to_str(tuple(emk))
+            #ciekawe = "no {}".format(str_tst)
+            #print(ciekawe.format(1,2,3))
+            #print(bing)
             #cur.execute("SELECT {},{} FROM {nazwaa}".format(nazwaa=nazwa))
             #myresult = cur.fetchall()
             #print(myresult)
             #for x in myresult:
                 #print(x)
             #INSERT , WISE CREATION
-        if opcja == 2:
 
+        if opcja == 2:
+                insert_data(data,nazwa)
+                """
                 #val = len(z)-1
                 str = ""
 
                 i=0
-                while i <= 1:
-                    if i < 1:
+
+                ilosc = int(len(drg))-1
+                while i <= ilosc:
+                    
+                    if i < ilosc:
                         str = str+drg[i]+ " " + x[i] + ","
                         i = i+1
                     if i == 1:
                         str = str + drg[i] + " " + x[i]
                         i= i+1
-                print(str)
+                #print(str)
+                print('Funkcja:',conv_to_parstr(drg))
                 beg = "CREATE TABLE {nazwaa} (id INT AUTO_INCREMENT PRIMARY KEY,".format(nazwaa=nazwa)+str+")"
                 sql_in = "INSERT INTO {name} ({str}) VALUES ()".format(name=nazwa, str=str)
-                print(beg)
-                print(sql_in)
+                #print(beg)
+                #print(sql_in)
+
                 #try:
                  #   cur.execute(beg)
                 #except:
                  #   print("fail")
-
+        """
         ## INPUTING
         if opcja == 3:
             name = input('Enter name: ')
